9cdc1be6d470630b70699b48ddbaa5d0f29664c2
Ryan Clanton <55164720+ryancinsight@users.noreply.github.com>
2024-02-12 07:40:26 -0500

    Update install-build-deps.sh
    Add msys2 windows compatibility

--- install-build-deps.sh
+++ install-build-deps.sh
@@ -36,7 +36,7 @@ gentoo-*)
   emerge-webrsync
   emerge dev-util/cmake
   ;;
-arch-* | archarm-* | artix-* | endeavouros-*)
+arch-* | archarm-* | artix-* | endeavouros-* | msys2-*)
   pacman -Sy
   pacman -S --needed --noconfirm base-devel cmake util-linux
   ;;
